35b8ef3ea61f9f317df0beeac908bc87233bb13c
LinYe2804 <168910220+LinYe2804@users.noreply.github.com>
2024-06-07 22:31:12 +0800

    Add files via upload

---
+++ SolxyFix公益工具箱/SolxyFix.py
@@ -0,0 +1,29 @@
+# Source Generated with Decompyle++
+# File: SolxyFix.pyc (Python 3.12)
+
+import time
+import random
+
+def main():
+    print('欢迎使用Solxy Fix公益开端工具箱')
+    print('软件作者：KUN X')
+    print('用途：用于网易花雨庭服务器开端使用！')
+    print('更新日志：版本：V1：团队研发的【Open CL Plus】正在测试中强大的防封！')
+    print('Solxy Fix Pro 更新为全托客户端！使用AIR RAM技术！功能强大建议购买！')
+    print('建议：此公益工具箱服务器延迟较高建议多尝试进服！')
+    print('警告：仅用于绿色玩家快速进入游戏，请勿用于非法用途！')
+    cookies_account = input('请输入Cookies账号：')
+    cookies_password = input('请输入Cookies密码：')
+    print(f'''服务器已使用{cookies_account}账号''')
+    time.sleep(5)
+    print('正在写入Solxy Fix Louancer Fx......')
+    time.sleep(2)
+    print('已写入【MC网易花雨庭】服务器')
+    time.sleep(10)
+    print('服务启动成功已绕过服务器“Kick！/?Remote Louancer{Fx}/socket;')
+    time.sleep(5)
+# WARNING: Decompyle incomplete
+
+if __name__ == '__main__':
+    main()
+    return None
